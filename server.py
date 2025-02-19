7156d071f085de9d1775ea15feca15c41e0bf490
pedro <pedrogalmarcarl@gmail.com>
2025-02-19 20:12:42 -0300

    Adiconando rota para exibir a URL pública

--- server.py
+++ server.py
@@ -1,5 +1,5 @@
 from flask import Flask, request, jsonify
-
+import os
 app = Flask(__name__)
 
 
@@ -8,6 +8,12 @@ def receber_dados():
     dados = request.get_json()
     print("Dados recebidos:", dados)
     return jsonify({"status": "sucesso", "dados_recebidos": dados})
+
+
+@app.route('/')
+def get_url():
+    url = os.environ.get('RAILWAY_URL', 'URL não encontrada')
+    return f"Sua URL é: {url}"
 
 
 if __name__ == '__main__':
